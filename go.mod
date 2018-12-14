82bd7b099a0aca1e89b1d8fac9b3310f1b34a258
voutasaurus <voutasaurus@gmail.com>
2018-12-13 23:52:46 -0800

    basic skeleton for web server with oauth login

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/voutasaurus/oauthtest
+
+go 1.12
+
+require (
+	cloud.google.com/go v0.34.0 // indirect
+	github.com/voutasaurus/env v0.1.0
+	golang.org/x/net v0.0.0-20181213202711-891ebc4b82d6 // indirect
+	golang.org/x/oauth2 v0.0.0-20181203162652-d668ce993890
+)
---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/voutasaurus/env"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+func main() {
+	logger := log.New(os.Stderr, "oauthtest: ", log.Llongfile|log.LstdFlags|log.LUTC)
+	logger.Println("starting...")
+
+	fatal := func(key string) {
+		logger.Fatalf("expected environment variable %q to be set", key)
+	}
+
+	// Your credentials should be obtained from the Google
+	// Developer Console (https://console.developers.google.com).
+	h := &oauthHandler{oauth2.Config{
+		ClientID:     env.Get("OAUTH_CLIENT_ID").Required(fatal),
+		ClientSecret: env.Get("OAUTH_CLIENT_SECRET").Required(fatal),
+		RedirectURL:  env.Get("BASE_URL").Required(fatal) + "/oauth-google-redirect",
+		Scopes:       []string{"profile", "email"},
+		Endpoint:     google.Endpoint,
+	}}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/login", h.handleLogin)
+	mux.HandleFunc("/oauth-google-redirect", h.handleRedirect)
+
+	logger.Println("serving on :8080")
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
+
+type oauthHandler struct {
+	oauth2.Config
+}
+
+// handleLogin will redirect the user to Google's consent page to ask for
+// permission for the scopes specified in h.Config.
+//
+// Use this when the user is not authenticated and the current GET request
+// requires authorization. For POSTS you should just fail and expect the user
+// to log on before posting.
+func (h *oauthHandler) handleLogin(w http.ResponseWriter, r *http.Request) {
+	// TODO: stuff current URL into state (so we can redirect back there
+	// when we're done)
+	// TODO: ensure protocol and domain are specified in the URL
+	// r.URL.String()
+	url := h.AuthCodeURL("state")
+	http.Redirect(w, r, url, 307)
+}
+
+// handleRedirect gets the redirect from Google OAuth with the authorization
+// codes, retrieves the scopes from the identity provider, issues a cookie, and
+// redirects to the original URL.
+func (h *oauthHandler) handleRedirect(w http.ResponseWriter, r *http.Request) {
+	// TODO: extract authorization-code from request
+	tok, err := h.Exchange(oauth2.NoContext, "authorization-code")
+	if err != nil {
+		http.Error(w, err.Error(), 401)
+		return
+	}
+	client := h.Client(context.Background(), tok)
+	client.Get("...") // TODO: get scopes?
+
+	// TODO: encrypt and issue cookie
+	home := "TODO: extract from state"
+	http.Redirect(w, r, home, 307)
+}
